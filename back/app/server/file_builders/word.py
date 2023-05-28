170f30aedc9b8460298cb756528c6fc014f9ea44
max <Zlyden-1@yandex.ru>
2023-05-29 00:43:58 +0400

    added basic pdf & advanced xlsx readers, created new endpoints

--- back/app/server/file_builders/word.py
+++ back/app/server/file_builders/word.py
@@ -5,31 +5,8 @@ from datetime import date
 from docx.enum.section import WD_SECTION, WD_ORIENT
 from docx.shared import Inches
 from docx.shared import RGBColor
-from googletrans import Translator
-from docx.shared import Cm
 
 import docx
-import pandas as pd
-
-
-def translate_table_columns(table):
-    """
-    Переводит названия столбцов таблицы с английского на русский язык.
-    :param table: таблица (Table object)
-    """
-    translation_dict = {"name": "Диагноз",
-                        "speciality": "Специальность",
-                        "prescriptions": "Назначения",
-                        "doctor_name": "Имя доктора",
-                        "protocol_name": "Отделение",
-                        "upload_date": "Дата загрузки"}
-
-    # перевод названий столбцов
-    for col_idx, cell in enumerate(table.rows[0].cells):
-        original_text = cell.text
-        if original_text in translation_dict:
-            cell.text = translation_dict[original_text]
-    # return table
 
 
 async def builder(filename: str, data: List[dict]) -> str:
@@ -46,22 +23,22 @@ async def builder(filename: str, data: List[dict]) -> str:
         column.width = Inches(2)
 
     for i, key in enumerate(data[0].keys()):
-        if key != 'code' and key != 'extra_prescriptions':
+        if key != 'Оценка' and key != 'Лишние назначения':
             heading_cells[i].text = key
             heading_cells[i].paragraphs[0].alignment = 1
     for item in data:
         row_cells = table.add_row().cells
         for i, key in enumerate(item.keys()):
-            if item["code"] == 2:
-                if key != 'code' and key != 'extra_prescriptions':
+            if item["Оценка"] == 'Избыточные назначения':
+                if key != 'Оценка' and key != 'Лишние назначения':
                     if type(item[key]) == list:
                         row_cells[i].text = '\n'.join(item[key])
                         row_cells[i].paragraphs[0].runs[0].font.color.rgb = RGBColor(255, 0, 0)
                     else:
                         row_cells[i].text = str(item[key])
                         row_cells[i].paragraphs[0].runs[0].font.color.rgb = RGBColor(255, 0, 0)
-                elif key == 'extra_prescriptions':
-                    cell = table.cell(1, 2)
+                elif key == 'Лишние назначения':
+                    cell = table.cell(data.index(item)+1, 7)
                     nested_table = cell.add_table(rows=1, cols=1)
                     nested_cell = nested_table.cell(0, 0)
                     nested_cell.text = '\n'.join(item[key])
@@ -70,7 +47,7 @@ async def builder(filename: str, data: List[dict]) -> str:
                 else:
                     ...
             else:
-                if key != 'code':
+                if key != 'Оценка':
                     if type(item[key]) == list:
                         row_cells[i].text = '\n'.join(item[key])
                     else:
@@ -78,17 +55,13 @@ async def builder(filename: str, data: List[dict]) -> str:
 
     # удаляем ячейки в четвертом и пятом столбцах
     for row in table.rows:
-        row.cells[3]._element.clear()  # четвертый столбец
-        row.cells[4]._element.clear()  # пятый столбец
+        row.cells[8]._element.clear()  # четвертый столбец
+        row.cells[9]._element.clear()  # пятый столбец
 
-    # удаляем четвертый и пятый столбцы
+    # # удаляем четвертый и пятый столбцы
     for row in table.rows:
-        row._element.remove(row._element.tc_lst[3])  # четвертый столбец
-        row._element.remove(row._element.tc_lst[3])  # пятый столбец
-
-    # переводим названия столбцов для каждой таблицы в документе
-    for table in doc.tables:
-        translate_table_columns(table)
+        row._element.remove(row._element.tc_lst[8])  # четвертый столбец
+        row._element.remove(row._element.tc_lst[8])  # пятый столбец
 
     filename = find_free_filename(filename)
     doc.save(f'../media/{filename}')
@@ -108,24 +81,32 @@ if __name__ == '__main__':
     import asyncio
 
     print(asyncio.run(builder('test', [
-        {
-            "name": "Вазомоторный ринит",
-            "speciality": "Отоларингология",
-            "prescriptions": ["Флюорография легких", "Электрокардиография в покое"],
-            "extra_prescriptions": ["Креатинин"],
-            "code": 2,
-            "doctor_name": 'Иван Иванович',
-            "protocol_name": "Отоларингологическое отделение",
-            "upload_date": date(1900, 1, 1)
-        },
-        {
-            "name": "Вазомоторный ринит",
-            "speciality": "Отоларингология",
-            "prescriptions": ["Флюорография легких", "Электрокардиография в покое"],
-            "extra_prescriptions": [],
-            "code": 0,
-            "doctor_name": 'Иван Петрович',
-            "protocol_name": "Отоларингологическое отделение",
-            "upload_date": date(1900, 2, 1),
-        }
-    ])))
+                {
+                    'Пол пациента': 'Муж',
+                    'Дата рождения пациента': date(2000, 1, 1),
+                    'ID пациента': 3,
+                    'Код МКБ-10': 'J32.9',
+                    "Диагноз": "Вазомоторный ринит",
+                    'Дата оказания услуги': date(2000, 1, 1),
+                    'Должность': 'врач-оториноларинголог',
+                    "Назначения": ["Флюорография легких", "Электрокардиография в покое"],
+                    "Лишние назначения": ["Креатинин"],
+                    "Оценка": "Избыточные назначения",
+                    "Источник данных": "Ортоларингологическое_отделение.xlsx",
+                    "Дата загрузки": date(2000, 1, 1)
+                },
+                {
+                    'Пол пациента': 'Муж',
+                    'Дата рождения пациента': date(2000, 1, 1),
+                    'ID пациента': 3,
+                    'Код МКБ-10': 'J32.9',
+                    "Диагноз": "Вазомоторный ринит",
+                    'Дата оказания услуги': date(2000, 1, 1),
+                    'Должность': 'врач-оториноларинголог',
+                    "Назначения": ["Флюорография легких", "Электрокардиография в покое"],
+                    "Лишние назначения": ['ХУЙ'],
+                    "Оценка": "Избыточные назначения",
+                    "Источник данных": "Ортоларингологическое_отделение.xlsx",
+                    "Дата загрузки": date(2000, 2, 1),
+                }
+            ])))
