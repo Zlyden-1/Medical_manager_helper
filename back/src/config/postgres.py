3d6bdee17f0f364fde311ea884eb17a2d3c33fcd
Zlyden-1 <Zlyden-1@yandex.ru>
2024-12-27 01:32:58 +0400

    feat: add postgres connection, config and abc for sqlalchemy models

---
+++ back/src/config/postgres.py
@@ -0,0 +1,26 @@
+from pydantic_settings import BaseSettings
+from sqlalchemy import URL
+
+
+class PostgresConfig(BaseSettings):
+    user: str = 'postgres'
+    host: str = 'postgres'
+    port: int = 5432
+    db: str = 'postgres'
+    driver: str = 'postgresql+asyncpg'
+    password: str = 'postgres'
+    ddl_show: bool = False
+
+    @property
+    def connection_url(self) -> URL:
+        return URL.create(
+            drivername=self.driver,
+            username=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+            database=self.db,
+        )
+
+
+POSTGRES_CONFIG = PostgresConfig(_env_prefix='POSTGRES_')
