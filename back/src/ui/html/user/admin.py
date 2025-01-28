6ff5460d1e098f49a7e5a89e2e45c2f66c847abd
Zlyden-1 <Zlyden-1@yandex.ru>
2025-01-28 05:43:04 +0400

    fix: fix create new user

--- back/src/ui/html/user/admin.py
+++ back/src/ui/html/user/admin.py
@@ -1,8 +1,30 @@
 from sqladmin import ModelView
+
+from src.domain.authentication.service import Hasher
 from src.domain.user.model import UserModel
 
 
 class UserAdmin(ModelView, model=UserModel):
+    name = "Пользователь"
+    name_plural = "Пользователи"
+    icon = "fa-solid fa-user"
+
+    form_create_rules = [
+        "email",
+        "first_name",
+        "surname",
+        "patronymic",
+        "birthdate",
+        "password",
+    ]
+    form_edit_rules = [
+        "email",
+        "first_name",
+        "surname",
+        "patronymic",
+        "birthdate",
+    ]
+
     column_list = [
         UserModel.id,
         UserModel.email,
@@ -11,3 +33,7 @@ class UserAdmin(ModelView, model=UserModel):
         UserModel.patronymic,
         UserModel.birthdate,
     ]
+
+    async def on_model_change(self, data, model, is_created, request) -> None:
+        if is_created:
+            data['password'] = Hasher.get_password_hash(data['password'])
